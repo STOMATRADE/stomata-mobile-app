807772dba101625d88f21fb8329189365ff85aed
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2025-11-29 01:42:03 +0700

    cleaninig code

--- lib/features/portofolio/controller/portofolio_controller.dart
+++ lib/features/portofolio/controller/portofolio_controller.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import 'package:stomata_app/features/portofolio_detail/controller/portofolio_detail_controller.dart';
 import 'package:stomata_app/features/portofolio_detail/portofolio_detail_screen.dart';
 
 class PortofolioController extends GetxController {
